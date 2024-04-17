71732f06678e3fbd6cf263178006b7324c4c02b2
Niladri2003 <niladri.adak.21@gmail.com>
2024-04-17 22:07:00 +0530

    some refactor in indexfile and controllers are separated

--- model/temperetatureSchema.js
+++ model/temperetatureSchema.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const environmentalDataSchema = new mongoose.Schema({
     humidity: { type: Number, default: 0 },
     temperature: { type: Number, default: 0 },
-    lastUpdatedTimestamp: { type: Date, default: Date.now },
     timestamp: { type: Date, default: Date.now }
 });
 
